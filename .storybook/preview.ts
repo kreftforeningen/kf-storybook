9c18685ff90828872a25179b93985aabf8fc86cb
Asbjørn Ness <asbjorn.ness@gmail.com>
2025-06-20 09:14:19 +0200

    Set order

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -2,6 +2,16 @@ import type { Preview } from "@storybook/react-vite";
 
 const preview: Preview = {
   parameters: {
+    options: {
+      storySort: {
+        order: [
+          "kf-tailwind",
+          ["Introduction", "Colors", "Fonts", "Headings", "Text"],
+          "kf-components",
+          "shadcnui-components",
+        ],
+      },
+    },
     controls: {
       matchers: {
         color: /(background|color)$/i,
